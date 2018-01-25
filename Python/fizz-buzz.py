98971a1b0094987fcf64bb0afe7f171c8f3da12d
Aven Xiangwen Liu <CMWENLIU@GMAIL.COM>
2018-01-25 17:01:34 -0600

    Update fizz-buzz.py

--- Python/fizz-buzz.py
+++ Python/fizz-buzz.py
@@ -6,28 +6,13 @@
 # But for multiples of three it should output “Fizz” instead of the number and for
 # the multiples of five output “Buzz”. For numbers which are multiples of both three
 # and five output “FizzBuzz”.
-#
 # Example:
-#
 # n = 15,
 #
 # Return:
 # [
-#     "1",
-#     "2",
-#     "Fizz",
-#     "4",
-#     "Buzz",
-#     "Fizz",
-#     "7",
-#     "8",
-#     "Fizz",
-#     "Buzz",
-#     "11",
-#     "Fizz",
-#     "13",
-#     "14",
-#     "FizzBuzz"
+#     "1", "2","Fizz","4", "Buzz",
+#     "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"
 # ]
 
 class Solution(object):
