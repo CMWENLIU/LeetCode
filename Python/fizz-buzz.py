c3c6fbc40b53e405c6a90cbbcea322f7e3c6749c
Aven Xiangwen Liu <CMWENLIU@GMAIL.COM>
2018-01-25 17:03:48 -0600

    Update fizz-buzz.py

--- Python/fizz-buzz.py
+++ Python/fizz-buzz.py
@@ -1,20 +1,10 @@
-# Time:  O(n)
-# Space: O(1)
-
+# Time:  O(n)  # Space: O(1)
 # Write a program that outputs the string representation of numbers from 1 to n.
-#
 # But for multiples of three it should output “Fizz” instead of the number and for
 # the multiples of five output “Buzz”. For numbers which are multiples of both three
 # and five output “FizzBuzz”.
-# Example:
-# n = 15,
-#
-# Return:
-# [
-#     "1", "2","Fizz","4", "Buzz",
-#     "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"
-# ]
-
+# Example:# n = 15,# Return:
+# [  "1", "2","Fizz","4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" ]
 class Solution(object):
     def fizzBuzz(self, n):
         """
@@ -22,7 +12,6 @@ class Solution(object):
         :rtype: List[str]
         """
         result = []
-
         for i in xrange(1, n+1):
             if i % 15 == 0:
                 result.append("FizzBuzz")
