737ee96cec0b6be73458377e0e3dbe27dd8c7e6d
cmwenliu <cmwenliu@gmail.com>
2018-02-21 13:28:32 -0600

    .

--- Python/find-all-numbers-disappeared-in-an-array.py
+++ Python/find-all-numbers-disappeared-in-an-array.py
@@ -10,13 +10,10 @@
 # You may assume the returned list does not count as extra space.
 #
 # Example:
-#
 # Input:
 # [4,3,2,7,8,2,3,1]
-#
 # Output:
 # [5,6]
-
 
 class Solution(object):
     def findDisappearedNumbers(self, nums):
